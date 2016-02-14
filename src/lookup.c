872fe1810801ccfbd94b86acbb7928f917c3b194
Mathieu Lirzin <mthl@gnu.org>
2016-02-14 19:44:28 +0100

    maint: Update copyright notices.
    * include/cache.h: Update copyright notice to follow the guidelines
    described at <https://www.gnu.org/licenses/gpl-howto.html>.
    * include/http.h: Likewise.
    * include/init.h: Likewise.
    * include/jconfig.h: Likewise.
    * include/jwhois.h: Likewise.
    * include/lookup.h: Likewise.
    * include/rwhois.h: Likewise.
    * include/utils.h: Likewise.
    * include/whois.h: Likewise.
    * src/cache.c: Likewise.
    * src/http.c: Likewise.
    * src/init.c: Likewise.
    * src/jconfig.c: Likewise.
    * src/jwhois.c: Likewise.
    * src/lookup.c: Likewise.
    * src/rwhois.c: Likewise.
    * src/utils.c: Likewise.
    * src/whois.c: Likewise.

--- src/lookup.c
+++ src/lookup.c
@@ -1,21 +1,21 @@
 /*
-    This file is part of jwhois
-    Copyright (C) 1999,2001-2003, 2007, 2015, 2016  Free Software Foundation, Inc.
+   Copyright (C) 1999, 2001-2003, 2007, 2015, 2016 Free Software Foundation, Inc.
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 3 of the License, or
-    (at your option) any later version.
+   This file is part of GNU JWhois.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+   GNU JWhois is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
 
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-*/
+   GNU JWhois is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with GNU JWhois.  If not, see <http://www.gnu.org/licenses/>.  */
+
 #ifdef HAVE_CONFIG_H
 # include <config.h>
 #endif
