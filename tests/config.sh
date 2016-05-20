05f7f69cb587c9029ef3e6d5febf8658698016c7
Mathieu Lirzin <mthl@gnu.org>
2016-05-20 16:43:00 +0200

    build: Import 'test-framework-sh' module from Gnulib.
    * bootstrap.conf (gnulib_modules): Add 'test-framework-sh'.
    * tests/config.sh: New test.
    * Makefile.am (TEST_EXTENSIONS, SH_LOG_COMPILER, AM_TESTS_ENVIRONMENT)
    (TESTS): New variables.
    (EXTRA_DIST): Add 'TESTS' and 'tests/init.sh'.

---
+++ tests/config.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# Check "--config" command line argument.
+# Copyright (C) 2016 Free Software Foundation, Inc.
+#
+# This file is part of GNU JWhois.
+#
+# GNU JWhois is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# GNU JWhois is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with GNU JWhois.  If not, see <http://www.gnu.org/licenses/>.
+
+. "${srcdir=.}/tests/init.sh"
+
+jwhois --config=${confdir}/jwhois.conf gnu.org || fail=1
+
+jwhois -c ${confdir}/jwhois.conf gnu.org || fail=1
+
+Exit $fail
