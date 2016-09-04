befb4cc62eba13e2478f9457f40febdafae6b459
Mathieu Lirzin <mthl@gnu.org>
2016-09-04 16:13:18 +0200

    maint: Configure emacs automatically.
    * .dir-locals.el: New file.

---
+++ .dir-locals.el
@@ -0,0 +1,11 @@
+;; Per-directory local variables for GNU Emacs 23 and later.
+
+((nil
+  . ((fill-column . 78)
+     (tab-width   .  8)))
+ (c-mode
+  . ((indent-tabs-mode . nil)
+     (c-file-style . "gnu")))
+ (texinfo-mode
+  . ((indent-tabs-mode . nil)
+     (fill-column . 72))))
